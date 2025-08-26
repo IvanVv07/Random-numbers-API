6a5369ec5afbf67278f3763596140ad1d3a5a8f8
Ivan <a2021116557@estudiantes.upsa.edu.bo>
2025-08-25 20:50:09 -0400

    creando las carpeta models y el codigo

---
+++ Models/RandomRaquest.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomApi.Models;
+
+public class RandomRequest
+{
+    public string? Type { get; set; }          // "number" | "decimal" | "string"
+    public int? Min { get; set; }              // solo si type=number
+    public int? Max { get; set; }              // solo si type=number
+    public int? Decimals { get; set; }         // solo si type=decimal
+    public int? Length { get; set; }           // solo si type=string
+}
